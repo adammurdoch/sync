575d3c821155d59567b7c8ccf5ee9503e477f73b
Adam Murdoch <adam@gradle.com>
2024-12-02 08:42:14 +1100

    Refactor app.

--- src/main/kotlin/net/rubygrapefruit/sync/DirTree.kt
+++ src/main/kotlin/net/rubygrapefruit/sync/DirTree.kt
@@ -2,4 +2,22 @@ package net.rubygrapefruit.sync
 
 import net.rubygrapefruit.file.Directory
 
-class DirTree(val dir: Directory)
+sealed class TreeEntry {
+    abstract val name: String
+    abstract val count: Int
+}
+
+class DirTree(val dir: Directory, val entries: List<TreeEntry>) : TreeEntry() {
+    override val name: String
+        get() = dir.name
+
+    override val count: Int
+        get() {
+            return 1 + entries.sumOf { entry -> entry.count }
+        }
+}
+
+class RegularFileEntry(override val name: String) : TreeEntry() {
+    override val count: Int
+        get() = 1
+}
--- src/main/kotlin/net/rubygrapefruit/sync/SyncApp.kt
+++ src/main/kotlin/net/rubygrapefruit/sync/SyncApp.kt
@@ -1,12 +1,15 @@
 package net.rubygrapefruit.sync
 
 import net.rubygrapefruit.cli.app.CliApp
+import net.rubygrapefruit.file.Directory
+import net.rubygrapefruit.file.ElementType
 import net.rubygrapefruit.file.fileSystem
 import net.rubygrapefruit.store.Store
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.locks.ReentrantLock
 
-class SyncApp: CliApp("sync") {
+class SyncApp : CliApp("sync") {
     private val local by dir().parameter("local", help = "Local directory")
 
     override fun run() {
@@ -15,13 +18,27 @@ class SyncApp: CliApp("sync") {
         Store.open(storeDir).use { store ->
             val index = store.map<String, FileHash>("details")
             val stateLock = ReentrantLock()
-            val executor = Executors.newFixedThreadPool(4)
-            val result = executor.submit {
-                DirTree(local)
+            val executor = Executors.newCachedThreadPool()
+            val result = executor.submit<DirTree> {
+                visitDir(local, executor)
             }
-            result.get()
+            val tree = result.get()
+            Logger.info("Found entries: ${tree.count}")
         }
         Logger.info("Sync finished")
+    }
+
+    private fun visitDir(directory: Directory, executor: ExecutorService): DirTree {
+        val entries = directory.listEntries().map { entry ->
+            executor.submit<TreeEntry> {
+                when (entry.type) {
+                    ElementType.RegularFile -> RegularFileEntry(entry.name)
+                    ElementType.Directory -> visitDir(entry.toDir(), executor)
+                    else -> throw UnsupportedOperationException("Unsupported type ${entry.type} for $entry")
+                }
+            }
+        }.map { it.get() }
+        return DirTree(directory, entries)
     }
 }
 
