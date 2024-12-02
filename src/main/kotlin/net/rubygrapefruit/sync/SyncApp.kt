e2c0ee14916f23ece6eccbef216b9a43b67f5a37
Adam Murdoch <adam@gradle.com>
2024-12-02 16:21:49 +1100

    Refactor app.

--- src/main/kotlin/net/rubygrapefruit/sync/SyncApp.kt
+++ src/main/kotlin/net/rubygrapefruit/sync/SyncApp.kt
@@ -3,6 +3,7 @@ package net.rubygrapefruit.sync
 import net.rubygrapefruit.cli.app.CliApp
 import net.rubygrapefruit.file.Directory
 import net.rubygrapefruit.file.ElementType
+import net.rubygrapefruit.file.RegularFile
 import net.rubygrapefruit.file.fileSystem
 import net.rubygrapefruit.store.Store
 import java.util.concurrent.Executors
@@ -38,14 +39,22 @@ class SyncApp : CliApp("sync") {
             if (node == null) {
                 return
             }
-            val entries = node.directory.listEntries()
-            queue.visiting(node) {
-                for (entry in entries) {
-                    when (entry.type) {
-                        ElementType.Directory -> queue.add(node.dir(entry.toDir()))
-                        ElementType.RegularFile -> node.regularFile(entry.name)
-                        ElementType.SymLink -> node.symLink(entry.name)
-                        else -> throw UnsupportedOperationException("Unsupported type ${entry.type} for $entry")
+            when (node) {
+                is RegularFileNode -> {
+                    queue.visiting(node) {}
+                }
+
+                is DirectoryNode -> {
+                    val entries = node.directory.listEntries()
+                    queue.visiting(node) {
+                        for (entry in entries) {
+                            when (entry.type) {
+                                ElementType.Directory -> queue.add(node.dir(entry.toDir()))
+                                ElementType.RegularFile -> queue.add(node.regularFile(entry.toFile()))
+                                ElementType.SymLink -> node.symLink(entry.name)
+                                else -> throw UnsupportedOperationException("Unsupported type ${entry.type} for $entry")
+                            }
+                        }
                     }
                 }
             }
@@ -103,27 +112,38 @@ class SyncApp : CliApp("sync") {
         }
     }
 
-    sealed class Node(val directory: Directory) {
+    sealed class Node {
+        abstract fun visited()
+    }
+
+    class RegularFileNode(val file: RegularFile, val parent: DirectoryNode) : Node() {
+        override fun visited() {
+            parent.childFinished(RegularFileEntry(file.name))
+        }
+    }
+
+    sealed class DirectoryNode(val directory: Directory) : Node() {
         private var visited = false
-        private var waitingForDirs = 0
+        private var waitingForChildren = 0
         private val entries = mutableListOf<TreeEntry>()
 
-        val finished: Boolean get() = visited && waitingForDirs == 0
+        val finished: Boolean get() = visited && waitingForChildren == 0
 
         fun dir(directory: Directory): Node {
-            waitingForDirs++
+            waitingForChildren++
             return ChildNode(directory, this)
         }
 
-        fun regularFile(name: String) {
-            entries += RegularFileEntry(name)
+        fun regularFile(file: RegularFile): RegularFileNode {
+            waitingForChildren++
+            return RegularFileNode(file, this)
         }
 
         fun symLink(name: String) {
             entries += SymlinkEntry(name)
         }
 
-        fun visited() {
+        override fun visited() {
             require(!visited)
             visited = true
             if (finished) {
@@ -131,9 +151,9 @@ class SyncApp : CliApp("sync") {
             }
         }
 
-        fun childFinished(entry: DirTree) {
-            require(visited && waitingForDirs > 0)
-            waitingForDirs--
+        fun childFinished(entry: TreeEntry) {
+            require(visited && waitingForChildren > 0)
+            waitingForChildren--
             entries += entry
             if (finished) {
                 finished()
@@ -147,7 +167,7 @@ class SyncApp : CliApp("sync") {
         abstract fun finished(tree: DirTree)
     }
 
-    class RootNode(directory: Directory) : Node(directory) {
+    class RootNode(directory: Directory) : DirectoryNode(directory) {
         var result: DirTree? = null
 
         override fun finished(tree: DirTree) {
@@ -155,7 +175,7 @@ class SyncApp : CliApp("sync") {
         }
     }
 
-    class ChildNode(directory: Directory, private val parent: Node) : Node(directory) {
+    class ChildNode(directory: Directory, private val parent: DirectoryNode) : DirectoryNode(directory) {
         override fun finished(tree: DirTree) {
             parent.childFinished(tree)
         }
