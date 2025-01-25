b6e21c6a74292d97d457ceaab9e1ee3124fffd19
Adam Murdoch <a@rubygrapefruit.net>
2025-01-26 08:53:32 +1100

    Upgrade Kotlin tools.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ application {
     targetJavaVersion = 21
 
     dependencies {
-        implementation("net.rubygrapefruit.libs:strings:1.0")
-        implementation("net.rubygrapefruit.libs:cli-app:1.0")
-        implementation("net.rubygrapefruit.libs:file-io:1.0")
-        implementation("net.rubygrapefruit.libs:store:1.0")
+        implementation("net.rubygrapefruit:basics:1.0")
+        implementation("net.rubygrapefruit:cli-app:1.0")
+        implementation("net.rubygrapefruit:file-io:1.0")
+        implementation("net.rubygrapefruit:store:1.0")
     }
 }
